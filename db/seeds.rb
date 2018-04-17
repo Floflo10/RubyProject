87821d22da9fd7b2e943f93c34bcccfc11f4f5f7
Ismael <ismael.hadjarab@ynov.com>
2018-04-17 17:09:36 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,7 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+   user = User.create({ email: 'ismael.hadj13@gmail.com', password: "password" })
+   theme = Theme.create({ name: 'aventura name', genre: "winter" })
+   trips = Trip.create({ goal: 'Tanzani', time: 12, cost: 150, story:"juste un voyage", user: user, theme: theme })
 #   Character.create(name: 'Luke', movie: movies.first)
