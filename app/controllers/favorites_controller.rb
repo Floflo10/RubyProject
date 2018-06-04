1cb4aca113072801043204e1ab0473af06b5f059
florian.martines@ynovaix.com <florian.martines@ynov.com>
2018-06-04 15:57:43 +0200

    Cloud

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
 
   def index
-    @travels = Favorite.all
+    @favorite = Favorite.all
   end
 
   def new
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,11 +1,3 @@
-# @Author: Ismael Hadj
-# @Date:   2018-05-21T10:46:10+02:00
-# @Email:  Ismael.hadj13@yahoo.com
-# @Last modified by:   Ismael Hadj
-# @Last modified time: 2018-05-21T16:17:57+02:00
-
-
-
 class PictureUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 end
